ab68e3693d54985ab17b5c77e5b7afa58dd64c5e
leeinwha <88917793+leeinwha@users.noreply.github.com>
2021-12-30 02:44:17 +0900

    finish

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const items = document.querySelector('.items');
+const form = document.querySelector('.new-form');
 const input = document.querySelector('.footer__input');
 const addBtn = document.querySelector('.footer__button');
 
+form.addEventListener('submit' , (event) => {
+  event.preventDefault();
+  onAdd();
+});
 function onAdd() {
   // 1. 사용자가 입력한 텍스트를 받아온다
   const text = input.value;
@@ -37,18 +42,18 @@ function createItem(text) {
   return itemRow;
 }
 
-addBtn.addEventListener('click', () => {
-  onAdd();
-});
+// addBtn.addEventListener('click', () => {
+//   onAdd();
+// });
 
-input.addEventListener('keydown', event => {
-  if (event.isComposing) {
-    return;
-  }
-  if (event.key === 'Enter') {
-    onAdd();
-  }
-});
+// input.addEventListener('keydown', event => {
+//   if (event.isComposing) {
+//     return;
+//   }
+//   if (event.key === 'Enter') {
+//     onAdd();
+//   }
+// });
 
 items.addEventListener('click', event => {
   const id = event.target.dataset.id;
